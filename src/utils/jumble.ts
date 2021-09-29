367e11f8cabf7cf5a23b4ae9e563b97a94e5e719
Keshav Mohta <xkeshav@gmail.com>
2021-09-29 22:23:00 +0530

    refactor

--- src/utils/jumble.ts
+++ src/utils/jumble.ts
@@ -1,6 +1,6 @@
 
 
-export {};
+export { };
 
 const input = 'torzeonthowoereezero';
 
@@ -12,7 +12,7 @@ type Numeral = {
 }
 
 const numeral: Numeral = {
-    'zero' : 0,
+    'zero': 0,
     'one': 1,
     'two': 2,
     'three': 3,
@@ -21,10 +21,10 @@ const numeral: Numeral = {
     'six': 6,
     'seven': 7,
     'eight': 8,
-    'nine':9
+    'nine': 9
 };
 
-const indexFound = (word: string, str: string[]) =>  str.indexOf(word) !== -1;
+const indexFound = (word: string, str: string[]) => str.indexOf(word) !== -1;
 
 // check whether every letter exist on given string
 const isExist = (word: any, str: any) => [...word].every((w) => indexFound(w, str));
@@ -36,22 +36,22 @@ const replacedWord = (word: string, str: string) => [...word].reduce(replaceLett
 
 // check the occurrence of numeral letter in given string
 
-const findOccurrence = ([sentence, collector]: [string, number[]], word: string) : [string, number[]] => {
-	const exist = isExist(word, sentence);
-	if(exist) {
-	    collector.push((numeral[word])); // push respective number in separate array
-		const replacedString  = replacedWord(word, sentence); // replace match word from input string
-		return [replacedString, collector];
-	} else {
+const findOccurrence = ([sentence, collector]: [string, number[]], word: string): [string, number[]] => {
+    const exist = isExist(word, sentence);
+    if (exist) {
+        collector.push((numeral[word])); // push respective number in separate array
+        const replacedString = replacedWord(word, sentence); // replace match word from input string
+        return [replacedString, collector];
+    } else {
         return [sentence, collector];
-	}
+    }
 }
 
 
 const findJumbleWord = (input: string, next: number[] = []) => {
     // tricky part is we are sending array as initial value so that if there is multiple instance of the same literal then it will be done on next time calling of same method
     const [remains] = Object.keys(numeral).reduce(findOccurrence, [input, next]);
-    if(remains.length > 2 ) { // as we know no numeral is less than 3 letter
+    if (remains.length > 2) { // as we know no numeral is less than 3 letter
         findJumbleWord(remains, next);
     }
     return next;
@@ -61,6 +61,6 @@ const result = findJumbleWord(input);
 
 // now if we require to sort ascending and display as a number then
 
-const resultSorted =  result.sort().join('');
+const resultSorted = result.sort().join('');
 
-console.log({result, resultSorted});
+console.log({ result, resultSorted });
